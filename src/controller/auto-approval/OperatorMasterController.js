66d59330c377928ecfdb8bb0d49ec14aea836c8a
Ankita <scsit.dev4@sparecare.in>
2025-09-19 12:42:29 +0530

    operator api

---
+++ src/controller/auto-approval/OperatorMasterController.js
@@ -0,0 +1,15 @@
+import {
+ getOperator
+} from "../../services/auto-approval/OperatorMasterService.js";
+import { ApiError } from "../../utils/ApiError.js";
+
+const fetchOperators = async (req, res, next) => {
+  try {
+    const result = await getOperator();
+    res.status(201).json({ success: true, ...result });
+  } catch (err) {
+    next(new ApiError(400, "cannot fetch the data"));
+  }
+};
+
+export {fetchOperators};
--- src/routes/auto-approval/index.js
+++ src/routes/auto-approval/index.js
@@ -2,12 +2,17 @@ import { Router } from "express";
 import bucketroutes from "./bucket-managementRoute.js";
 import parameterroutes from "./parameter-managementRoute.js"
 import spmuploadroutes from "./spm-upload.js"
+import userModuleConfigroutes from './user-module-configRoutes.js'
+import ruleroutes from './rule-managementRoute.js'
+import operatorroutes from './OperatorRoutes.js'
 
 const router = Router()
-
 
 router.use('/aa',bucketroutes)
 router.use('/aa',parameterroutes)
 router.use('/aa',spmuploadroutes)
+router.use('/aa',userModuleConfigroutes)
+router.use('/aa',ruleroutes)
+router.use('/aa',operatorroutes)
 
 export default router
